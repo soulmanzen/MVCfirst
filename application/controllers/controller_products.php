ceb9e0442bdfae27ad9f4d992606e2d968606f6e
soulmanzen <soulmanzen@gmail.com>
2017-03-29 19:40:46 +0300

    lesson16

---
+++ application/controllers/controller_products.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soulman
+ * Date: 25.03.17
+ * Time: 15:10
+ */
+
+class ControllerProducts extends Controller{
+
+    public function show(){
+        var_dump(123);
+        die();
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soulman
+ * Date: 16.03.17
+ * Time: 19:28
+ */
+
+ini_set('display_errors', 1);
+require_once '../application/bootstrap.php';
